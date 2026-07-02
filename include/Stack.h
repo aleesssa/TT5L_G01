4bf8b4d140e3e51b590ad62797ab7632b78a2029
qsrhzrn <qaisarah.azran@gmail.com>
2026-07-02 20:06:29 +0800

    Add Stack.h

---
+++ include/Stack.h
@@ -0,0 +1,19 @@
+#ifndef STACK_H
+#define STACK_H
+
+class Stack
+{
+private:
+    signed char stack[8];
+    int top;
+
+public:
+    Stack();
+    bool isEmpty();
+    bool isFull();
+    void push(signed char value);
+    signed char pop();
+    signed char peek();
+};
+
+#endif
