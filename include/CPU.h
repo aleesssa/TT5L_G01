ea30949740dde3a9520a94fb5fe9bbfdf28a8e52
qsrhzrn <qaisarah.azran@gmail.com>
2026-07-02 21:16:24 +0800

    Update CPU.h
    Updated CPU class to include additional methods for register and memory manipulation, as well as flag management.

--- include/CPU.h
+++ include/CPU.h
@@ -9,23 +9,49 @@
 class CPU
 {
 private:
-    GeneralRegister reg[8];
+
+    GeneralRegister registers[8];
+
     Memory memory;
+
     Stack stack;
+
     FlagRegister flags;
+
     unsigned char PC;
     unsigned char SI;
 
 public:
+
     CPU();
-    GeneralRegister& getRegister(int i);
-    Memory& getMemory();
-    Stack& getStack();
-    FlagRegister& getFlags();
+
+    void setRegisterValue(int index, signed char value);
+    signed char getRegisterValue(int index);
+
+    void writeMemory(int address, signed char value);
+    signed char readMemory(int address);
+
+    void pushStack(signed char value);
+    signed char popStack();
+
+    void setZeroFlag(bool value);
+    bool getZeroFlag();
+
+    void setCarryFlag(bool value);
+    bool getCarryFlag();
+
+    void setOverflowFlag(bool value);
+    bool getOverflowFlag();
+
+    void setUnderflowFlag(bool value);
+    bool getUnderflowFlag();
+
+    void setPC(int value);
     int getPC();
     void incrementPC();
-    int getSI();
+
     void setSI(int value);
+    int getSI();
 };
 
 #endif
