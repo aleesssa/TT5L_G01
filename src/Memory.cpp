16c81ebba5eb9f2e238300457c85c410e8543fc5
qsrhzrn <qaisarah.azran@gmail.com>
2026-07-02 23:11:47 +0800

    Update Memory.cpp
    Added address validation to read and write methods.

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,4 +1,6 @@
 #include "Memory.h"
+#include <iostream>
+using namespace std;
 
 Memory::Memory()
 {
@@ -15,10 +17,20 @@ void Memory::clear()
 
 signed char Memory::read(int address)
 {
+    if(address < 0 || address >= 64)
+    {
+        cout << "Invalid memory address\n";
+        return;
+    }
     return mem[address];
 }
 
 void Memory::write(int address, signed char value)
 {
+    if(address < 0 || address >= 64)
+    {
+        cout << "Invalid memory address\n";
+        return;
+    }
     mem[address] = value;
 }
