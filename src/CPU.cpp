05838f36a82c6f0f8c5cd887be57e5123d74e819
qsrhzrn <qaisarah.azran@gmail.com>
2026-07-02 21:15:48 +0800

    Update CPU.cpp

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -2,37 +2,94 @@
 
 CPU::CPU()
 {
-    reg[0] = GeneralRegister("R0");
-    reg[1] = GeneralRegister("R1");
-    reg[2] = GeneralRegister("R2");
-    reg[3] = GeneralRegister("R3");
-    reg[4] = GeneralRegister("R4");
-    reg[5] = GeneralRegister("R5");
-    reg[6] = GeneralRegister("R6");
-    reg[7] = GeneralRegister("R7");
+    registers[0] = GeneralRegister("R0");
+    registers[1] = GeneralRegister("R1");
+    registers[2] = GeneralRegister("R2");
+    registers[3] = GeneralRegister("R3");
+    registers[4] = GeneralRegister("R4");
+    registers[5] = GeneralRegister("R5");
+    registers[6] = GeneralRegister("R6");
+    registers[7] = GeneralRegister("R7");
 
     PC = 0;
     SI = 0;
 }
 
-GeneralRegister& CPU::getRegister(int i)
+void CPU::setRegisterValue(int index, signed char value)
 {
-    return reg[i];
+    registers[index].setValue(value);
 }
 
-Memory& CPU::getMemory()
+signed char CPU::getRegisterValue(int index)
 {
-    return memory;
+    return registers[index].getValue();
 }
 
-Stack& CPU::getStack()
+void CPU::writeMemory(int address, signed char value)
 {
-    return stack;
+    memory.write(address, value);
 }
 
-FlagRegister& CPU::getFlags()
+signed char CPU::readMemory(int address)
 {
-    return flags;
+    return memory.read(address);
+}
+
+void CPU::pushStack(signed char value)
+{
+    stack.push(value);
+    SI++;
+}
+
+signed char CPU::popStack()
+{
+    SI--;
+    return stack.pop();
+}
+
+void CPU::setZeroFlag(bool value)
+{
+    flags.setZero(value);
+}
+
+bool CPU::getZeroFlag()
+{
+    return flags.getZero();
+}
+
+void CPU::setCarryFlag(bool value)
+{
+    flags.setCarry(value);
+}
+
+bool CPU::getCarryFlag()
+{
+    return flags.getCarry();
+}
+
+void CPU::setOverflowFlag(bool value)
+{
+    flags.setOverflow(value);
+}
+
+bool CPU::getOverflowFlag()
+{
+    return flags.getOverflow();
+}
+
+void CPU::setUnderflowFlag(bool value)
+{
+    flags.setUnderflow(value);
+}
+
+bool CPU::getUnderflowFlag()
+{
+    return flags.getUnderflow();
+}
+
+void CPU::setPC(int value)
+{
+    PC = value;
 }
 
 int CPU::getPC()
@@ -45,12 +102,12 @@ void CPU::incrementPC()
     PC++;
 }
 
-int CPU::getSI()
-{
-    return SI;
-}
-
 void CPU::setSI(int value)
 {
     SI = value;
+}
+
+int CPU::getSI()
+{
+    return SI;
 }
