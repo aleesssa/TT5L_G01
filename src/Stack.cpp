de53d03c57416eb5dc37e613dc60224b7a1bbb01
qsrhzrn <qaisarah.azran@gmail.com>
2026-07-02 20:07:36 +0800

    Add Stack.cpp

---
+++ src/Stack.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "Stack.h"
+using namespace std;
+
+Stack::Stack()
+{
+    top = -1;
+}
+
+bool Stack::isEmpty()
+{
+    if(top == -1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool Stack::isFull()
+{
+    if(top == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void Stack::push(signed char value)
+{
+    if(isFull())
+    {
+        cout << "Stack Overflow" << endl;
+        return;
+    }
+
+    top++;
+    stack[top] = value;
+}
+
+signed char Stack::pop()
+{
+    if(isEmpty())
+    {
+        cout << "Stack Underflow" << endl;
+        return;
+    }
+
+    signed char value;
+    value = stack[top];
+
+    top--;
+
+    return value;
+}
+
+signed char Stack::peek()
+{
+    if(isEmpty())
+    {
+        cout << "Stack is empty\n";
+        return 0;
+    }
+
+    return stack[top];
+}
